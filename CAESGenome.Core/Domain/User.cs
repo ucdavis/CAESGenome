4976d34c1e63f94f9221331132bb4129f4d6519a
anlai <anlai@ucdavis.edu>
2012-09-20 08:18:36 -0700

    +imported nuget packages
    fixed some problems wiht the first import of the nuget packages.
    successfully did it this time and setup everything so that it builds.

---
+++ CAESGenome.Core/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UCDArch.Core.DomainModel;
+
+namespace CAESGenome.Core.Domain
+{
+    public class User : DomainObject
+    {
+    }
+}
