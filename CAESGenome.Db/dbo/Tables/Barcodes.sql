cab4bae921660709d0d33210d6d4b7fb108dcf47
anlai <anlai@ucdavis.edu>
2012-09-20 09:46:17 -0700

    found relationship with barcodes!

--- CAESGenome.Db/dbo/Tables/Barcodes.sql
+++ CAESGenome.Db/dbo/Tables/Barcodes.sql
@@ -4,10 +4,11 @@
     [PlateSubId]    INT  NULL,
     [PrimerId]      INT  NULL,
     [Stage]         INT  NULL,
-    [SourceBarcode] INT  NULL,
+    [SourceBarcodeId] INT  NULL,
     [Date]          DATE NULL,
     [Done]          BIT  NULL,
     CONSTRAINT [PK_Barcodes] PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_Barcodes_Primers] FOREIGN KEY ([PrimerId]) REFERENCES [dbo].[Primers] ([Id])
+    CONSTRAINT [FK_Barcodes_Primers] FOREIGN KEY ([PrimerId]) REFERENCES [dbo].[Primers] ([Id]), 
+    CONSTRAINT [FK_Barcodes_Barcodes] FOREIGN KEY ([SourceBarcodeId]) REFERENCES [Barcodes]([Id])
 );
 
