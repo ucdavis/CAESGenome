5fa9a2338a577c1d232a1effc5f7d9e3deeb9c88
anlai <anlai@ucdavis.edu>
2012-09-20 08:49:53 -0700

    more visual changes preparation

---
+++ CAESGenome/Controllers/ApplicationController.cs
@@ -0,0 +1,17 @@
+﻿using UCDArch.Web.Controller;
+using UCDArch.Web.Attributes;
+
+namespace CAESGenome.Controllers
+{
+    [Version(MajorVersion = 1)]
+    public class ApplicationController : SuperController
+    {
+        public string ErrorMessage
+        {
+            get { return TempData[TEMP_DATA_ERROR_MESSAGE_KEY] as string; }
+            set { TempData[TEMP_DATA_ERROR_MESSAGE_KEY] = value; }
+        }
+
+        private const string TEMP_DATA_ERROR_MESSAGE_KEY = "ErrorMessage";
+    }
+}
--- CAESGenome/Controllers/HomeController.cs
+++ CAESGenome/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace CAESGenome.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : ApplicationController
     {
         public ActionResult Index()
         {
