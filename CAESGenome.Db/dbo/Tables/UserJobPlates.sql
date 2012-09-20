7120286afe401e43ac5e15563b82c07f486295f1
anlai <anlai@ucdavis.edu>
2012-09-20 09:11:52 -0700

    added userjob plates table

---
+++ CAESGenome.Db/dbo/Tables/UserJobPlates.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserJobPlates]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserJobId] INT NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL, 
+    CONSTRAINT [FK_UserJobPlates_UserJobs] FOREIGN KEY ([UserJobId]) REFERENCES [UserJobs]([Id])
+)
