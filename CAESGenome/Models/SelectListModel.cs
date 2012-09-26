a10db0e98c333b67469f785db6c1034a78690c59
anlai <anlai@ucdavis.edu>
2012-09-26 09:57:13 -0700

    custom select list

---
+++ CAESGenome/Models/SelectListModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace CAESGenome.Models
+{
+    public class SelectListModel
+    {
+        public SelectListModel(string name, SelectList data, string label = null)
+        {
+            Label = label;
+            Name = name;
+            Data = data;
+        }
+
+        public string Label { get; set; }
+        public string Name { get; set; }
+        public SelectList Data { get; set; }
+        
+        public string Id { get { return Name.Replace('.', '_'); } }
+    }
+}
