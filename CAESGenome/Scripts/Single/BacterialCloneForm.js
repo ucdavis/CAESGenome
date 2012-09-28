f076a9a4be49fca7fda63ae35ef4b6b7fa362f38
anlai <anlai@ucdavis.edu>
2012-09-28 12:53:21 -0700

    generic new strain

--- CAESGenome/Scripts/Single/BacterialCloneForm.js
+++ CAESGenome/Scripts/Single/BacterialCloneForm.js
@@ -1,14 +1,4 @@
 ﻿$(function () {
-    $("#PostModel_Strain").change(function (event) {
-        if ($(this).find("option:selected").html() == "other") {
-            $("#otherHost").show('normal');
-        } else {
-            $("#otherHost").hide('normal');
-            $("#PostModel_NewStrain").val('');
-            $("#PostModel_Bacteria").val('-1');
-        }
-    });
-
     // show/hide sequence direction
     $("#sequencedirections input").change(function () {
         if ($(this).val() == sequenceDirectionTwo) {
