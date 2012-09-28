7279c4abb267187633d0b3d4080f453dbe2ed3a7
anlai <anlai@ucdavis.edu>
2012-09-28 12:46:21 -0700

    public information

--- CAESGenome/Controllers/HomeController.cs
+++ CAESGenome/Controllers/HomeController.cs
@@ -10,22 +10,21 @@ namespace CAESGenome.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your app description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        public ActionResult Service()
+        {
+            return View();
+        }
+
+        public ActionResult Rates()
+        {
             return View();
         }
     }
