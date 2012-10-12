73aac120f6644e581383fb0e728a89566b5bd58d
anlai <anlai@ucdavis.edu>
2012-10-12 11:29:58 -0700

    bulk insert users

---
+++ CAESGenome.Db/Scripts/CopyData.sql
@@ -0,0 +1,20 @@
+﻿/*
+  Wipe any existing data, so we don't get conflicting keys
+*/
+
+delete from Barcodes
+delete from userjobplates
+delete from userjobs
+delete from UserProfilesXRechargeAccounts
+delete from RechargeAccounts
+delete from webpages_UsersInRoles
+delete from userprofile
+delete from webpages_Membership
+
+/* copy user profiles */
+
+-- set the username for those that are blank
+update [user]
+set username = firstname + '.' + lastname + '@fake.com'
+where username is null
+
