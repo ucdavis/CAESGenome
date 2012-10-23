5661a515165e35829573cc15a6f63c7b98a476ba
anlai <anlai@ucdavis.edu>
2012-10-22 19:38:04 -0700

    working through file upload

--- CAESGenome/Controllers/TestController.cs
+++ CAESGenome/Controllers/TestController.cs
@@ -1,5 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using UCDArch.Web.ActionResults;
 
 namespace CAESGenome.Controllers
 {
@@ -12,9 +17,33 @@ namespace CAESGenome.Controllers
 
         
         [HttpPost]
-        public ActionResult UploadFile(HttpPostedFileBase[] files)
+        public JsonNetResult UploadFile(HttpPostedFileBase file)
         {
-            return View();
+            var name = file.FileName;
+            var size = file.ContentLength;
+
+            var result = new List<FileUploadResult>();
+            result.Add(new FileUploadResult() {Name = name, Size = size});
+
+            return new JsonNetResult(result);
+        }
+
+        public FileResult DownloadFile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FileUploadResult
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string url {
+            get
+            {
+                var url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+                return url.Action("DownloadFile", "Test"); // Will output the proper link accordi
+            } 
         }
     }
 }
