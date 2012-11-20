384c5d2f8d4fe118a258840f827c1351923f627b
anlai <anlai@ucdavis.edu>
2012-11-20 12:42:01 -0800

    updated import scripts

---
+++ CAESGenome.Db/Scripts/ImportPhredQualities.sql
@@ -0,0 +1,24 @@
+﻿declare @cursor cursor
+declare @barcode int, @cellchar char(1), @cellnum int, @start int, @end int, @datetimesubmitted datetime
+
+set @cursor = cursor for
+	select top 100 barcode, cellchar, cellnum, start, [end], datetimesubmitted
+	from cgfold.dbo.quality_phred
+
+open @cursor
+
+fetch next from @cursor into @barcode, @cellchar, @cellnum, @start, @end, @datetimesubmitted
+
+while(@@FETCH_STATUS = 0)
+begin
+
+	update barcodefiles
+	set [start] = @start, [end] = @end, datetimeuploaded = @datetimesubmitted, datetimevalidated = @datetimesubmitted
+	where barcode = @barcode, wellrow = @cellchar, wellcolumn= @cellnum, 
+
+	fetch next from @cursor into @barcode, @cellchar, @cellnum, @start, @end, @datetimesubmitted
+
+end
+
+close @cursor
+deallocate @cursor
